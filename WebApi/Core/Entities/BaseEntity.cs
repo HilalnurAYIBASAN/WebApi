0703c13faa1c61cceacf81f549a5b54588f267bb
hilal25 <95569102+hilal25@users.noreply.github.com>
2024-03-26 08:02:43 +0300

    WebApi
    Kodalama.io 3.gün

---
+++ WebApi/Core/Entities/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities;
+
+public class BaseEntity<TId>//TypeId şeklinde kodlanmış.
+{
+    public TId Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+}
---
+++ WebApi/WebApi/Controllers/BrandsController.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstracts;
+using Business.Dtos.Requests;
+using Business.Dtos.Responses;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+        private readonly IBrandService _brandService;
+        public BrandsController(IBrandService brandService)
+        {
+            _brandService = _brandService;
+        }
+        [HttpPost]
+        public IActionResult Add(CreateBrandRequest createBrandRequest)
+        {
+            CreatedBrandResponse createdBrandResponse = _brandService.Add(createBrandRequest);
+
+            return Ok(createdBrandResponse);//200 demiş olduk ok ile
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_brandService.GetAll());
+        }
+    }
+}
+
+
+
+//200 başarılı demek
+//201 başarılı ve eklendi demek
+//400 hata demek
